327e3bb68adb51babd439b52241e7f8ae6fc553f
JordynHam <jordyn_business@outlook.com>
2025-12-01 22:35:42 -0500

    Updated main scene to include new features

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -5,7 +5,7 @@ public class RestartGame : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void LoadCurrentScene()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Nic-TestScene"); // loads to named scene
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main"); // loads to named scene
         Time.timeScale = 1; // unpauses game
     }
     
